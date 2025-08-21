ff95dd7d1e65b9952b2cb1c74b4a8f8b8181514b
Moses <bigdang42@gmail.com>
2025-08-21 19:14:06 +0100

    Rock, Paper and Scissors Game

--- ROCK_PAPER_SCISSORS/rock_paper_scissors_game.dart
+++ ROCK_PAPER_SCISSORS/rock_paper_scissors_game.dart
@@ -1,12 +1,44 @@
 // This is a command line app
 
 import 'dart:io';
+import 'dart:math';
+
+enum Move{ rock, paper, scissors}
 
 void main(){
+  final rnd = Random();
+  while(true){
   stdout.write('Rock, paper or scissors? (r/p/s) ');
   final input = stdin.readLineSync();
-  print(input);
+  if(input == 'r' || input == 'p' || input == 's' ){
+    var playerMove;
+    if(input == 'r'){
+      playerMove = Move.rock;
+    } else if (input == 'p'){
+      playerMove = Move.paper;
+    } else{
+      playerMove = Move.scissors;
+    }
+    final random = rnd.nextInt(3);
+    final aiMove = Move.values[random];
 
+    print('You played: $playerMove');
+    print('AI played: $aiMove');
+    if (playerMove == aiMove){
+      print('Its is a Draw');
+    } else if(playerMove == Move.rock && aiMove == Move.scissors ||
+    playerMove == Move.paper && aiMove == Move.paper ||
+    playerMove == Move.scissors && aiMove == Move.paper){
+      print('You Win');
+    }else {
+      print('You Lose');
+    }
+  } else if (input == 'q'){
+    break;
+  } else{
+    print('Invalid input');
+  }
+}
   // show prompt
   // read user input from console
   // if iput is a valid move(r.p or q)
