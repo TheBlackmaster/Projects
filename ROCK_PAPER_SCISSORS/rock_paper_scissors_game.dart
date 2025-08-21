af89f87a41011501f63e72b101a30ca79b2b4911
Moses <bigdang42@gmail.com>
2025-08-21 17:27:59 +0100

    Learnt how to use stdin and std out for the console through dart.io import

---
+++ ROCK_PAPER_SCISSORS/rock_paper_scissors_game.dart
@@ -0,0 +1,21 @@
+// This is a command line app
+
+import 'dart:io';
+
+void main(){
+  stdout.write('Rock, paper or scissors? (r/p/s) ');
+  final input = stdin.readLineSync();
+  print(input);
+
+  // show prompt
+  // read user input from console
+  // if iput is a valid move(r.p or q)
+  //  choose the ai move at random
+  //  compare player move with ai move
+  //  show result
+  // if input is q
+  //  quit program
+  // if anything else
+  //  show invalid input
+
+}
