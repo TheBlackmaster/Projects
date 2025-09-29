9e687c2015448fa61ba72dd8ac55a7dc7dfc695d
Moses <bigdang42@gmail.com>
2025-09-29 20:04:28 +0100

    adding items to the cart

--- COMMERCE_APP/checkOut.dart
+++ COMMERCE_APP/checkOut.dart
@@ -7,8 +7,8 @@ class Product {
 
   const Product({required this.id, required this.name, required this.price});
 
-  String get displayName => "\$($initial)${name.substring(1)}: \$$price";
-  String get initial => name.substring(0,1);
+  String get displayName => "($initial)${name.substring(1)}: \$$price";
+  String get initial => name.substring(0, 1);
 }
 
 class Item {
@@ -18,10 +18,38 @@ class Item {
   Item({required this.product, this.quantity = 1});
 
   double get price => quantity * product.price;
+
+  @override
+  String toString() => '$quantity x ${product.name}:\$$price';
 }
 
 class Cart {
-  // TODO: ITEMS COLLECTION
+  final Map<int, Item> _items = {};
+
+  void addProduct(Product product) {
+    final item = _items[product.id];
+    if (item == null) {
+      _items[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _items.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  @override
+  String toString() {
+    if (_items.isEmpty) {
+      return 'Cart is empty';
+    }
+
+    final itemizedList = _items.values
+        .map((item) => item.toString())
+        .join('\n');
+    return '--------\n$itemizedList\nTotal: \$${total()}\n--------';
+  }
 }
 
 const allProducts = [
@@ -46,18 +74,20 @@ const allProducts = [
 // end
 
 void main() {
+  final cart = Cart();
   while (true) {
     stdout.write(
       'What do you want to do? (v)iew items, (a)dd item, (c)heckout: ',
     );
     final line = stdin.readLineSync();
     if (line == 'a') {
-final product = chooseProduct();
-if(product != null){
-  print(product.displayName);
-}
+      final product = chooseProduct();
+      if (product != null) {
+        cart.addProduct(product);
+        print(cart);
+      }
     } else if (line == 'v') {
-      // TODO
+      print(cart);
     } else if (line == 'c') {
       // TODO
     }
@@ -71,8 +101,8 @@ Product? chooseProduct() {
 
   stdout.write('Available products: \n$productsList\nYour choice: ');
   final line = stdin.readLineSync();
-  for(var product in allProducts){
-    if(product.initial == line){
+  for (var product in allProducts) {
+    if (product.initial == line) {
       return product;
     }
   }
